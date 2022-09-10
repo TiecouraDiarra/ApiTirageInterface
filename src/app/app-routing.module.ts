cf896cb18c201864dc0745147b4ca4298227e529
tddiarra <tiecouradiarra1999@gmail.com>
2022-09-10 07:51:11 +0000

    Page accueil1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailslistesComponent } from './detailslistes/detailslistes.component';
+import { DetailstiragesComponent } from './detailstirages/detailstirages.component';
+import { TirageComponent } from './tirage/tirage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"accueil",
+    component:AccueilComponent
+  },
+  {
+    path:"",
+    redirectTo:"accueil",
+    pathMatch:'full'
+  },
+  {
+    path:"tirage",
+    component:TirageComponent
+  },
+  {
+    path:"detailtirage",
+    component:DetailstiragesComponent
+  },
+  {
+    path:"detailslistes",
+    component:DetailslistesComponent
+  },
+  
+  {
+    path:"dasboard",
+    component:DashboardComponent
+    
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
